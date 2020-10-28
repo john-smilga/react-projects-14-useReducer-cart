2c69fb0346c82cf1f0bed80244767a786075dee2
john-smilga <johnsmilga@gmail.com>
2020-10-27 21:05:37 -0700

    switched to permanent domain

--- src/context.js
+++ src/context.js
@@ -1,45 +1,49 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react';
-import cartItems from './data';
-import reducer from './reducer';
-const url = 'https://course-api.netlify.app/api/react-useReducer-cart-project';
-const AppContext = React.createContext();
+import React, { useState, useContext, useReducer, useEffect } from 'react'
+import cartItems from './data'
+import reducer from './reducer'
+
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-useReducer-cart-project'
+
+const AppContext = React.createContext()
 
 const initialState = {
   loading: false,
   cart: [],
   total: 0,
   amount: 0,
-};
+}
 
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const increase = (id) => {
-    dispatch({ type: 'INCREASE', payload: id });
-  };
+    dispatch({ type: 'INCREASE', payload: id })
+  }
   const decrease = (id) => {
-    dispatch({ type: 'DECREASE', payload: id });
-  };
+    dispatch({ type: 'DECREASE', payload: id })
+  }
   const remove = (id) => {
-    dispatch({ type: 'REMOVE', payload: id });
-  };
+    dispatch({ type: 'REMOVE', payload: id })
+  }
   const clearCart = () => {
-    dispatch({ type: 'CLEAR_CART' });
-  };
+    dispatch({ type: 'CLEAR_CART' })
+  }
   useEffect(() => {
-    dispatch({ type: 'GET_TOTALS' });
-  }, [state.cart]);
+    dispatch({ type: 'GET_TOTALS' })
+  }, [state.cart])
 
   const fetchData = async () => {
-    dispatch({ type: 'LOADING' });
-    const response = await fetch(url);
-    const cart = await response.json();
-    dispatch({ type: 'DISPLAY_ITEMS', payload: cart });
-  };
+    dispatch({ type: 'LOADING' })
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     <AppContext.Provider
@@ -53,11 +57,11 @@ const AppProvider = ({ children }) => {
     >
       {children}
     </AppContext.Provider>
-  );
-};
+  )
+}
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext);
-};
+  return useContext(AppContext)
+}
 
-export { AppContext, AppProvider };
+export { AppContext, AppProvider }
